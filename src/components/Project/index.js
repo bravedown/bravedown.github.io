dd0e922b997775ec3517f418cdcd6a3e1ee46f00
David <davidbrown3142@gmail.com>
2021-01-25 17:28:52 -0500

    repo links

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -12,7 +12,7 @@ function Project(props) {
         <Card.Img variant="top" src={props.src} />
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
-          <Card.Text>{props.text}</Card.Text>
+          <Card.Text>{props.text} You can view it <a href={props.github}>here</a>.</Card.Text>
         </Card.Body>
       </Card>
     </Col>
--- src/projects.js
+++ src/projects.js
@@ -4,32 +4,38 @@ const projects = [
     {
         src: './img/budget.png',
         title: 'Budget Tracker',
-        text: 'PWA to track your budget on or offline.'
+        text: 'PWA to track your budget on or offline.',
+        github: 'https://github.com/bravedown/budget-tracker'
     },
     {
         src: './img/employee.png',
         title: 'Employee Tracker',
-        text: 'Node.js command line app to track employees in a company.'
+        text: 'Node.js command line app to track employees in a company.',
+        github: 'https://github.com/bravedown/employee-manager'
     },
     {
         src: './img/goldview.png',
         title: 'Gold View',
-        text: 'App to view stock charts in terms of their value in gold over time.'
+        text: 'App to view stock charts in terms of their value in gold over time.',
+        github: 'https://github.com/bravedown/gold-view'
     },
     {
         src: './img/password.png',
         title: 'Password Generator',
-        text: 'App to generate secure passwords.'
+        text: 'App to generate secure passwords.',
+        github: 'https://github.com/bravedown/password-generator'
     },
     {
         src: './img/weather.png',
         title: 'Weather Dashboard',
-        text: 'App to view the weather forecast for the week by searching a city.'
+        text: 'App to view the weather forecast for the week by searching a city.',
+        github: 'https://github.com/bravedown/weather-dashboard'
     },
     {
         src: './img/workout.png',
         title: 'Workout Tracker',
-        text: 'App to track your workouts. Data is stored in MongoDB.'
+        text: 'App to track your workouts. Data is stored in MongoDB.',
+        github: 'https://github.com/bravedown/fitness-tracker'
     },
 ];
 
@@ -38,6 +44,7 @@ const ProjectElements = projects.map(e => (
       src={e.src}
       title={e.title}
       text={e.text}
+      github={e.github}
     />
 ));
 
